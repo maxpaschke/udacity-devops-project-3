ee51a1369412d1f90b3607266b34a94eabbf3d62
Max Paschke <max.paschke@de.bosch.com>
2022-07-14 12:03:42 +0200

    use ms install script

--- terraform/import_resource_group.sh
+++ terraform/import_resource_group.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
-curl -sL https://packages.microsoft.com/keys/microsoft.asc |
-    gpg --dearmor |
-    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    sudo tee /etc/apt/sources.list.d/azure-cli.list
-sudo apt-get update
-sudo apt-get install azure-cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 resourcegroupId=`az group list | python -c "import sys, json; print(json.load(sys.stdin)[0]['id'])"`
 echo $resourcegroupId
