f129966a05e8bcabaf2c5cd7e934029b8a03edf8
Max Paschke <max.paschke@de.bosch.com>
2022-07-14 10:51:05 +0200

    Fix RG import

--- terraform/import_resource_group.sh
+++ terraform/import_resource_group.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-resourcegroupId=az group list | python -c "import sys, json; print(json.load(sys.stdin)[0]['id'])"
-terraform import azurerm_resource_group.test $resourcegroupId
+resourcegroupId=$(az group list | python -c "import sys, json; print(json.load(sys.stdin)[0]['id'])")
+echo $resourcegroupId
+terraform import azurerm_resource_group.test resourcegroupId
