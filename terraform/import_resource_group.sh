066588918a37d7c2e495a59e6fe521c2cef22ba6
Max Paschke <max.paschke@de.bosch.com>
2022-07-14 11:59:47 +0200

    install azure cli

--- terraform/import_resource_group.sh
+++ terraform/import_resource_group.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
+
 resourcegroupId=`az group list | python -c "import sys, json; print(json.load(sys.stdin)[0]['id'])"`
 echo $resourcegroupId
 terraform import 'azurerm_resource_group.test' "$resourcegroupId"
