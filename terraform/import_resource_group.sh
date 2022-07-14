cc4f0da5a6eca7b120950fbaa27a571f4e829407
Max Paschke <max.paschke@de.bosch.com>
2022-07-14 10:44:37 +0200

    update the import script

--- terraform/import_resource_group.sh
+++ terraform/import_resource_group.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-resourcegroupId = az group list | python -c "import sys, json; print(json.load(sys.stdin)[0]['id'])"
+resourcegroupId=az group list | python -c "import sys, json; print(json.load(sys.stdin)[0]['id'])"
 terraform import azurerm_resource_group.test $resourcegroupId
