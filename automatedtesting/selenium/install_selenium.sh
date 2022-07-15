f1e524aa567fdf0d1cf239c0f19043a831ea9ee1
Max Paschke <max.paschke@de.bosch.com>
2022-07-15 10:55:16 +0200

    install

--- automatedtesting/selenium/install_selenium.sh
+++ automatedtesting/selenium/install_selenium.sh
@@ -3,6 +3,8 @@ echo "Run the installation"
 sudo apt-get upgrade -y
 sudo apt-get install unzip -y
 sudo apt-get install -y chromium-browser
+sudo apt-get install -y chromium-chromedriver
 pip3 install selenium
 
 chromium-browser --version
+chromedriver --version
