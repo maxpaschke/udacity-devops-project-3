ed0bf49ab1d428a655dabf47eec7f4e7c623190b
Max Paschke <max.paschke@de.bosch.com>
2022-09-02 13:13:46 +0200

    add log timestamps

--- automatedtesting/selenium/selenium_test.py
+++ automatedtesting/selenium/selenium_test.py
@@ -1,6 +1,8 @@
 # #!/usr/bin/env python
 import imp
+import datetime
 from lib2to3.pgen2 import driver
+import string
 from attr import s
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,7 +13,7 @@ from selenium.webdriver.common.keys import Keys
 
 
 def login(user, password):
-    print('Starting the browser...')
+    log('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = Options()
     options.add_argument("--headless")
@@ -20,10 +22,10 @@ def login(user, password):
     # # Local version
     # driver = webdriver.Chrome()
 
-    print('Browser started successfully. Navigating to the demo page to login.')
+    log('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
     # Enter the login data
-    print(f"Logging in with ${user} and ${password}")
+    log(f"Logging in with ${user} and ${password}")
     driver.find_element(
         By.CSS_SELECTOR, "input[id='user-name']").send_keys(user)
     driver.find_element(
@@ -35,13 +37,13 @@ def login(user, password):
     loginsucessfull = num_shopelements == 6
     assert loginsucessfull
     if(loginsucessfull):
-        print(
+        log(
             f"Login sucessfull, found {num_shopelements} items after logging in with Username {user} and Password {password}")
     return driver
 
 
 def add_and_remove_cartitems(driver):
-    print("Adding and removing all items to and from the cart.")
+    log("Adding and removing all items to and from the cart.")
     items = driver.find_elements(By.CSS_SELECTOR, "div[class=inventory_item]")
     current_number_of_items = 0
     for item in items:
@@ -49,30 +51,30 @@ def add_and_remove_cartitems(driver):
         # Find the name of the element
         name = item.find_element(
             By.CSS_SELECTOR, "div[class=inventory_item_name]").text
-        print(f"Adding item `${name}` to the shopping cart.")
+        log(f"Adding item `${name}` to the shopping cart.")
         item.find_element(By.CSS_SELECTOR, "button.btn_inventory").click()
 
         shoppingcartstatus = find_cart_status(driver)
         assert int(shoppingcartstatus) == current_number_of_items
-        print(f"{shoppingcartstatus} item(s) in shopping cart.")
+        log(f"{shoppingcartstatus} item(s) in shopping cart.")
 
-    print("Remove the items from the shopping cart.")
+    log("Remove the items from the shopping cart.")
     for item in items:
         current_number_of_items -= 1
         # Find the name of the element
         name = item.find_element(
             By.CSS_SELECTOR, "div[class=inventory_item_name]").text
-        print(f"Removing item `${name}` from the shopping cart.")
+        log(f"Removing item `${name}` from the shopping cart.")
         item.find_element(By.CSS_SELECTOR, "button.btn_inventory").click()
 
         if(current_number_of_items > 0):
             shoppingcartstatus = find_cart_status(driver)
             assert int(shoppingcartstatus) == current_number_of_items
-            print(f"{shoppingcartstatus} item(s) in shopping cart.")
+            log(f"{shoppingcartstatus} item(s) in shopping cart.")
         else:
             assert len(driver.find_elements(
                 By.CSS_SELECTOR, "span[class=shopping_cart_badge")) == current_number_of_items
-            print("Shopping cart empty.")
+            log("Shopping cart empty.")
 
 
 def find_cart_status(driver):
@@ -82,8 +84,13 @@ def find_cart_status(driver):
     return shoppingcartstatus
 
 
+def log(input: str):
+    datestring = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    print(datestring+': ' + input)
+
+
 if __name__ == "__main__":
-    print("Running UI tests for saucedemo.com demo website.")
+    log("Running UI tests for saucedemo.com demo website.")
     driver = login('standard_user', 'secret_sauce')
     add_and_remove_cartitems(driver)
-    print("Selenium test sucessfully finished.")
+    log("Selenium test sucessfully finished.")
